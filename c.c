e7b885f51e9a0cf4801dcf446552898b698d34fa
Saradindu Sengupta <saradindusengupta285@gmail.com>
2016-05-13 13:43:19 +0530

    Create c.c

---
+++ c.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+ int i=0;
+ char ch[100],ch1[110];
+ printf("Enter name of the file : ");
+ scanf("%s", ch);
+ 
+ strcpy(ch1,"gedit ");
+ strcat(ch1,ch);
+printf("%s",ch1);
+ i=system(ch1);
+if (i!=0)
+ printf("\nOperation not successful");
+else
+ {
+  strcpy(ch1,"cc ");
+  strcat(ch1,ch);
+  system(ch1);
+  system("./a.out");
+ }
+ return 0;
+}
